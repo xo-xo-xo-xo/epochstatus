fe9183aa8ed2816580975399410cd635fb86cc28
xo-xo-xo-xo <93801988+xo-xo-xo-xo@users.noreply.github.com>
2025-08-01 13:59:49 -0700

    Update epochstatus.sh

--- epochstatus.sh
+++ epochstatus.sh
@@ -8,7 +8,7 @@ while true; do
     game_status=$(nc -z -w 5 198.244.179.121 8085 >/dev/null 2>&1 && echo "alive" || echo "fucked")
     timestamp=$(date '+%Y-%m-%d %H:%M:%S')
     echo "$timestamp auth server:3724 $auth_status"
-    echo "$timestamp kezan server:8085 $game_status"
+    echo "$timestamp kezan :8085 $game_status"
     [ "$auth_status" = "alive" ] && [ "$game_status" = "alive" ] && paplay "$SOUND_FILE" >/dev/null 2>&1
     sleep $DELAY
 done
